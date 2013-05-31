8ea188032bf4c3284442497f21a352e52c9d1f63
kiorky <kiorky@cryptelium.net>
2013-05-31 18:27:51 +0200

    odxport

--- hooks/setenv.py
+++ hooks/setenv.py
@@ -8,14 +8,18 @@ except:
 
 def getopenldapenv(options,buildout):
     #on everything else than linux patch configure too
+    if uname in ['FreeBSD']:
+        cmd1 = ("gsed "
+                " -e 's|-lsasl2||'" 
+                " -e 's|-lsasl||'" 
+                " -i %s/configure") % (options['compile-directory'])
+    if uname in ['Darwin']:
+        cmd1 = ("sed -iE 's/-lsasl2//g'  %(c)s/configure;" 
+                "sed -iE 's/-lsasl//g'   %(c)s/configure;")%{'c':options['compile-directory']}
     if uname in ['Darwin', 'FreeBSD']:
-        cmd1 = "gsed \
-                -e 's|-lsasl2||' \
-                -e 's|-lsasl||' \
-                -i %s/configure" % (options['compile-directory'])
-
+        print "Running %s" % cmd1
         if os.system(cmd1):
-                raise Error('System error')
+            raise Exception('System error: %s' % cmd1)
 
     if uname in ['Linux']:
         os.environ['CPPFLAGS'] = '%s %s' % (os.environ.get('CPPFLAGS', ''), ' -D_GNU_SOURCE ')
